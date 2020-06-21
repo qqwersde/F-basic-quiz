c2fa1f65721cacae2964717725f93590c6a507a9
Zhonghao Yin <zhonghao.yin@thoughtworks.com>
2020-06-21 18:33:18 +0800

    add container style

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import "./style/index.scss";
 // eslint-disable-next-line no-unused-vars
-import { getNameById } from "./Task1/getName";
+import { getNameById } from "./GetUser/getName";
 
 getNameById(1);
