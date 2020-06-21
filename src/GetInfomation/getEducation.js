bcd7137e2cec0135e87ef97cfffeadd112019eba
Zhonghao Yin <zhonghao.yin@thoughtworks.com>
2020-06-21 23:54:10 +0800

    get user education imfo by id

---
+++ src/GetInfomation/getEducation.js
@@ -0,0 +1,26 @@
+export const getEducationById = (id) => {
+  const address = "http://localhost:3000/users/";
+  const ed = "/educations";
+  const url = `${address}${id}${ed}`;
+  // eslint-disable-next-line no-unused-vars,no-new
+  const promise = new Promise(function (resolve, reject) {
+    fetch(url, { method: "GET" })
+      .then((response) => {
+        return response.json();
+      })
+      // eslint-disable-next-line no-unused-vars
+      .then((data) => {
+        if (data.code === 404) {
+          // eslint-disable-next-line prefer-promise-reject-errors
+          reject(`request exception${data.msg}`);
+        } else {
+          resolve(data);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  return promise;
+};
--- src/app.js
+++ src/app.js
@@ -1,15 +1,24 @@
 import "./style/index.scss";
 // eslint-disable-next-line no-unused-vars,import/named
 import { getNameById } from "./GetUser/getName";
+// import { getEducationById } from "./GetInfomation/getEducation";
 
 // eslint-disable-next-line no-unused-vars
 const test = window.location.pathname;
 // eslint-disable-next-line no-unused-vars
-const id = test.split("/")[2];
-
-const result = getNameById(id);
+const userId = test.split("/")[2];
+const resultName = getNameById(userId);
 // eslint-disable-next-line no-unused-vars
-result.then((data) => {
+resultName.then((data) => {
+  const { avatar } = data;
+  // eslint-disable-next-line no-undef
+  $("img").attr("src", avatar);
+  const { name } = data;
+  const { age } = data;
+  // eslint-disable-next-line no-undef
+  $("#header").append(
+    `<p>MY NAME IS ${name} ${age}YO.THESE IS MY RESUME/CV</p>`
+  );
   const { description } = data;
   // eslint-disable-next-line no-undef
   $(`#intromyself`).html(description);
